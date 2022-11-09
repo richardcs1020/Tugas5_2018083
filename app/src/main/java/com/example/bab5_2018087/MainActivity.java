f951ee63710e98441f5ab6d8c92f9fa54922f812
richitirta <richitirta@gmail.com>
2022-11-09 14:40:43 +0700

    tugas5

---
+++ app/src/main/java/com/example/bab5_2018087/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.bab5_2018087;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    // tugas recycle view
+    RecyclerView RecylerView;
+    String s1[], s2[],s3[];
+    int images[] = {R.drawable.kulkass,R.drawable.magic_com,R.drawable.mesin_cuci};
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abut;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // tugas recycle view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.Elektronik);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        ElektronikAdapter appAdapter = new ElektronikAdapter(this, s1, s2, s3, images);
+        RecylerView.setAdapter(appAdapter);
+        LinearLayoutManager layoutManager  = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        RecylerView.setLayoutManager(layoutManager);
+        RecylerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abut = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abut.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abut);
+        abut.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_profile) {
+                    Intent a = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_chat) {
+                    Intent a = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_message) {
+                    Intent a = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(a);
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abut.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
